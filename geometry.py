cf33495490b07bdc02ff2f3e9be268b646626119
Lando <lando.schumpich@gmail.com>
2021-11-02 15:13:45 +0100

    Implement Plane primitive

--- geometry.py
+++ geometry.py
@@ -1,13 +1,143 @@
-
+import numpy as np
 
 class Plane():
+    """
+    A Plane primitive.
+    It is internally represented as a parametrical matrix: [U, V, N, Q],
+    where Q is the origin and u, v, n are the x, y, z axes.
+    This allows for easy transforming from carthesian to plane space and back.
+    """
 
     def __init__(self, origin, x_axis, y_axis):
-        self.origin = origin
-        self.x_axis = x_axis
-        self.y_axis = y_axis
+        """
+        Creates a new plane instance.
 
-class Transformation():
+        Args:
+            origin (array-like): The position where the plane will be located.
+            x_axis (array-like): The x-axis direction of the plane. Needs to be normalized.
+            y_axis (array-like): The y-axis direction of the plane. Needs to be normalized.
+        """
+        normal = np.cross(x_axis, y_axis)
 
-    def __init__(self):
-        raise NotImplementedError()
+        matrix = np.array([x_axis, y_axis, normal, origin])
+        matrix = matrix.transpose()
+        self.__matrix = np.vstack([matrix, np.array([0, 0, 0, 1])])
+
+    @property
+    def x_axis(self):
+        """
+        The x-axis of the plane
+
+        Returns:
+            array-like: The x-axis
+        """
+
+        return self.__matrix[0, :-1]
+
+    @property
+    def y_axis(self):
+        """
+        The y_axis of the plane
+
+        Returns:
+            array-like: The y-axis
+        """
+
+        return self.__matrix[1, :-1]
+
+    @property
+    def z_axis(self):
+        """
+        The z_axis of the plan
+
+        Returns:
+            array-like: The z_axis
+        """
+
+        return self.__matrix[2, :-1]
+
+    @property
+    def origin(self):
+        """
+        The origin location of the plane
+
+        Returns:
+            array-like: The origin
+        """
+
+        return self.__matrix[3, :-1]
+
+    @staticmethod
+    def __to_homogenous(carthesian_vector):
+        """
+        Converts a carthesion vector (x, y, z) to a homogenous one (x, y, z, 1).
+        This is mainly used to allow Matrix operations with 4x4 Matrices.
+
+        Args:
+            carthesian_vector (array-like): The vector to convert
+
+        Returns:
+            array-like: The vector in homogenous coordinates.
+        """
+        return np.append(carthesian_vector, [1])
+
+    @staticmethod
+    def __to_carthesian(homogenous_vector):
+        """
+        Converts a homogenous vdctor (x, y, z, 1) to a carthesion vector (x, y, z), by omitting the last value.
+
+        Args:
+            homogenous_vector (array-like): The vector to convert
+
+        Returns:
+            array-like: The vector in carthesian coordinates.
+        """
+        return homogenous_vector[:-1]
+
+    def __evaluate(self, u, v, n):
+        """
+        Evaluate the plane at the given coordinates in plane space.
+
+        Args:
+            u (float): x-coordinate in plane space
+            v (float): y-coordinate in plane space
+            n (float): z-coordinate in plane space
+
+        Returns:
+            array-like: The evaluated point in carthesian space.
+        """
+
+        return self.__matrix.dot(
+            Plane.__to_homogenous(
+                np.array([u, v, n])))
+
+    def point_at(self, u, v, n):
+        """
+        Evaluate a point at the given coordinates in plane space.
+
+        Args:
+            u (float): x-coordinate in plane space
+            v (float): y-coordinate in plane space
+            n (float): z-coordinate in plane space
+
+        Returns:
+            array-like: The evaluated point in carthesian space.
+        """
+        
+        return Plane.__to_carthesian(self.__evaluate(u, v, n))
+
+    def convert_to_plane_space(self, vector):
+        """
+        Converts a point from carthesian to plane space.
+
+        Args:
+            vector (array-like): The point to convert.
+
+        Returns:
+            array-like: A point in plane coordinates
+        """
+        
+        inverse = np.linalg.inv(self.__matrix)
+        return Plane.__to_carthesian(
+            inverse.dot(
+                Plane.__to_homogenous(vector)))
---
+++ test_geometry.py
@@ -0,0 +1,29 @@
+import unittest
+import logging
+import numpy as np
+from geometry import Plane
+
+class TestFaceTopology(unittest.TestCase):
+    
+    def test_plane(self):
+        plane = Plane(np.array([0, 0, 0]), np.array([1, 0, 0]), np.array([0, 1, 0]))
+
+        np.testing.assert_array_almost_equal(np.array([0.5, 1.5, 2.5]), plane.point_at(0.5, 1.5, 2.5))
+
+        test_pt = np.array([0.5, 2.7, -1.7])
+        projected = plane.convert_to_plane_space(test_pt)
+
+        np.testing.assert_array_almost_equal(test_pt, projected)
+
+        plane = Plane(np.array([0, 0, 0]), np.array([1, 0, 0]), np.array([0, 0, 1]))
+
+        test_pt = np.array([-1.4, 3.2, -2.8])
+        projected = plane.convert_to_plane_space(test_pt)
+
+        np.testing.assert_array_almost_equal(np.array([-1.4, -2.8, -3.2]), projected)
+        np.testing.assert_array_almost_equal(test_pt, plane.point_at(projected[0], projected[1], projected[2]))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='test_output/test_geometry.log', filemode='w', level=logging.INFO)
+    unittest.main()
